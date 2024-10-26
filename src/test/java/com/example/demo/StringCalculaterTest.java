9e3a8000a261e1e5a7545f71250b74fe422c7828
raviawhad <raviawhad143@gmail.com>
2024-10-26 15:27:48 +0530

    Test feature2 Pass

--- src/test/java/com/example/demo/StringCalculaterTest.java
+++ src/test/java/com/example/demo/StringCalculaterTest.java
@@ -8,9 +8,13 @@ import static org.junit.Assert.assertEquals;
 
 public class StringCalculaterTest {
 		 @Test
-		    public void testSimpleAdditionAndSubtraction() {
-		        StringCalculater calculator = new StringCalculater();
-		        Assert.assertEquals(6, calculator.calculater("3+5-2"));
+		  public void testSimpleAdditionAndSubtraction() {
+		     StringCalculater calculator = new StringCalculater();
+		      Assert.assertEquals(6, calculator.calculater("3+5-2"));
 		    }
-	    
+		 @Test
+		    public void testMultipleDigitNumbers() {
+		        StringCalculater calculator = new StringCalculater();
+		        assertEquals(25, calculator.calculater("10+20-5"));
+		    }
 }
